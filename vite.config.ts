3d2aab2e18c9c0c73a6615d8b8c9ba6a71579059
Lincoln Anderson <lincolnanderson@gmail.com>
2025-07-31 18:35:05 -0400

    update project config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    allowedHosts: [
+      "a7c7-2600-8807-c186-f900-9d1b-9e70-b178-5447.ngrok-free.app",
+    ],
     port: 8080,
     open: false,
   },
